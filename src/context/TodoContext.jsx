f0edde19dfedac7789491166a727903a9ffa362f
Bobur92 <50585415+Bobur92@users.noreply.github.com>
2024-08-22 02:52:19 +0900

    Remove the json server!

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,67 +1,53 @@
-import { createContext, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { createContext, useState } from "react";
 
 const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
-  const [isloading, setIsloading] = useState(true);
-  const [todo, setTodo] = useState([]);
-  const [compleated, setCompleated] = useState([]);
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "Task1",
+      detail: "Make a bad",
+    },
+    {
+      id: 2,
+      title: "Task2",
+      detail: "Make a breakfast",
+    },
+    {
+      id: 3,
+      title: "Task3",
+      detail: "Do the dishes",
+    },
+  ]);
 
+  const [compleated, setCompleated] = useState([]);
   const [todoEdit, setTodoEdit] = useState({
     item: {},
     edit: false,
   });
 
-  useEffect(() => {
-    fetchTodo();
-  }, []);
-
-  //Fetch todo
-  const fetchTodo = async () => {
-    const response = await fetch(
-      "http://localhost:5000/Todo?_sort=id&_order=desc"
-    );
-    const data = await response.json();
-
-    setTodo(data);
-    setIsloading(false);
-  };
   //Add Task
-  const addTask = async (newTask) => {
-    const response = await fetch("http://localhost:5000/Todo", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(newTask),
-    });
-    const data = await response.json();
-    setTodo([data, ...todo]);
+  const addTask = (newTask) => {
+    newTask.id = uuidv4();
+    setTodo([newTask, ...todo]);
   };
   //Delete Task
   const deleteTask = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
-      await fetch(`http://localhost:5000/Todo/${id}`, { method: "DELETE" });
       setTodo(todo.filter((item) => item.id !== id));
     }
   };
   //Moving to the list of compleated tasks
 
   //Update Todo Item
-  const updateTodo = async (id, updTodo) => {
-    const response = await fetch(`http://localhost:5000/Todo/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updTodo),
-    });
-
-    const data = await response.json();
-
-    setTodo(todo.map((item) => (item.id === id ? { ...item, ...data } : item)));
+  const updateTodo = (id, updTodo) => {
+    setTodo(
+      todo.map((item) => (item.id === id ? { ...item, ...updTodo } : item))
+    );
   };
-
+  //Set items to be updated
   const editTodo = (item) => {
     setTodoEdit({
       item,
@@ -75,7 +61,6 @@ export const TodoProvider = ({ children }) => {
         todo,
         compleated,
         todoEdit,
-        isloading,
         deleteTask,
         addTask,
         editTodo,
